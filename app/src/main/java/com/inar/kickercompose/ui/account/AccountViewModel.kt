37a17ed5732ca86b7a80db0364d27b7f64fe33e2
Инар Инарыч <Inar2000s@mail.ru>
2022-04-21 12:28:05 +0300

    pip

---
+++ app/src/main/java/com/inar/kickercompose/ui/account/AccountViewModel.kt
@@ -0,0 +1,10 @@
+package com.inar.kickercompose.ui.account
+
+import androidx.lifecycle.ViewModel
+
+class AccountViewModel : ViewModel() {
+    val token: String = ""
+    val isLogged = false
+
+
+}
---
+++ app/src/main/java/com/inar/kickercompose/ui/account/LoginPage.kt
@@ -0,0 +1,11 @@
+package com.inar.kickercompose.ui.account
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginPage() {
+
+    Text(text = "Login")
+
+}
